90695e163f8eb05db2cce591b087fc7111c124c2
unknown <rory.saxby@gmail.com>
2020-11-10 22:31:58 +0000

    vibrate test

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-console.log('wooooooooooo')
+(() =>{
+    
+    const vibrate = () =>{
+        navigator.vibrate(2000);
+    }
+
+    const initVibrate = () =>{
+        const vibInt = setInterval(vibrate, 3000);
+    }
+
+    const stopVibrate = () =>{
+        setTimeout(()=>{
+            clearInterval(vibInt)
+        }, 3000)
+    }
+
+    setTimeout(()=>{
+        initVibrate();
+    }, 3000)
+})();
